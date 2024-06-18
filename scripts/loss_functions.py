e66d4c6fb901a05ea754b14a422351fbc7db70dc
niels <nielss.bijl@gmail.com>
2024-06-18 11:28:06 +0200

    documentation

--- scripts/loss_functions.py
+++ scripts/loss_functions.py
@@ -2,22 +2,41 @@ import torch
 
 
 def compute_class_weights(labels):
+    """
+    Compute weights for each class to address class imbalance.
+
+    Args:
+        labels (torch.Tensor): A tensor of class labels.
+
+    Returns:
+        torch.Tensor: A tensor of computed class weights.
+    """
     class_counts = torch.bincount(labels)
     class_weights = 1. / class_counts.float()
     class_weights /= class_weights.min()
-
-    # TODO: try more weight or remove
-    # Increase weight for class 1 by 10%
-    # class_weights[1] *= 1.10
-
     return class_weights
 
 
 class MoMLoss(torch.nn.Module):
+    """
+    Majority or Minority Loss function for handling data imbalance in Named Entity Recognition (NER) tasks.
+
+    Nemoto, S., Kitada, S., & Iyatomi, H. (2024). Majority or Minority: Data Imbalance Learning Method for Named Entity Recognition. arXiv preprint arXiv:2401.11431.
+    """
     def __init__(self):
         super(MoMLoss, self).__init__()
 
     def forward(self, logits, targets):
+        """
+        Compute the forward pass for the loss function.
+
+        Args:
+            logits (torch.Tensor): Predicted logits from the model, expected to be of shape (batch_size, num_classes).
+            targets (torch.Tensor): Ground truth labels, expected to be of shape (batch_size).
+
+        Returns:
+            torch.Tensor: The mean weighted loss.
+        """
         # Flatten predictions and labels
         logits = logits.view(-1, 2)  # Assuming 2 classes
         targets = targets.view(-1)
@@ -36,6 +55,9 @@ class MoMLoss(torch.nn.Module):
 
 
 class DiceLoss(torch.nn.Module):
+    """
+    Dice Loss function for handling data imbalance in Named Entity Recognition.
+    """
     def __init__(self, smooth=1.): 
         super(DiceLoss, self).__init__()
         self.smooth = smooth
